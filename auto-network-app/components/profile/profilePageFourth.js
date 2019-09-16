34db09d6ba80a0fb2ab55b88a9921290ca382673
poojan1109 <poojandharaiya@gmail.com>
2019-09-16 21:48:01 +0530

    image uploading practice

--- auto-network-app/components/profile/profilePageFourth.js
+++ auto-network-app/components/profile/profilePageFourth.js
@@ -17,41 +17,8 @@ import {
 import * as ImagePicker from "expo-image-picker";
 import Constants from "expo-constants";
 import * as Permissions from "expo-permissions";
-import RNFetchBlob from 'rn-fetch-blob'
 
-const storage=firebase.storage();
-const Blob = RNFetchBlob.polyfill.Blob
-const fs = RNFetchBlob.fs
-window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
-window.Blob = Blob
 
-const uploadImage = (uri, mime = 'application/octet-stream') => {
-  return new Promise((resolve, reject) => {
-    const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri
-      const sessionId = new Date().getTime()
-      let uploadBlob = null
-      const imageRef = storage.ref('images').child(`${sessionId}`)
-
-      fs.readFile(uploadUri, 'base64')
-      .then((data) => {
-        return Blob.build(data, { type: `${mime};BASE64` })
-      })
-      .then((blob) => {
-        uploadBlob = blob
-        return imageRef.put(blob, { contentType: mime })
-      })
-      .then(() => {
-        uploadBlob.close()
-        return imageRef.getDownloadURL()
-      })
-      .then((url) => {
-        resolve(url)
-      })
-      .catch((error) => {
-        reject(error)
-      })
-  })
-}
 
 var options = [{ label: "Yes", value: 0 }, { label: "No", value: 1 }];
 export default class profilePageFourth extends React.Component {
@@ -65,13 +32,13 @@ export default class profilePageFourth extends React.Component {
     this.skipEvent = this.skipEvent.bind(this);
     this.nextEvent = this.nextEvent.bind(this);
     this.uriToBlob = this.uriToBlob.bind(this);
-
   }
   componentWillMount(){
     let storageRef = firebase.storage().ref();
 
   }
   submitDetails(){
+
     const {navigation} = this.props;
     let user = navigation.getParam('user');
 
@@ -80,7 +47,7 @@ export default class profilePageFourth extends React.Component {
     };
     let image = this.state.image;
     var storageRef = firebase.storage().ref();
-    var uploadTask = storageRef.child('images/profile_pic.jpg').put(image, metadata);
+    var uploadTask = storageRef.child('Images/profile_pic.jpg').put(image, metadata);
     uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
       console.log('File available at', downloadURL);
     });
@@ -108,10 +75,8 @@ export default class profilePageFourth extends React.Component {
   }
  
 
-
-
   skipEvent(e){
-    
+
     const {navigation} = this.props;
     firebase.database().ref('Drivers/'+ user.uid).set({
       first_name: navigation.getParam('first_name'),
@@ -130,9 +95,66 @@ export default class profilePageFourth extends React.Component {
     
     this.props.navigation.navigate('mainScreen');
   }
+
   nextEvent(e){
-    this. uploadImage(this.state.image);
+    this._pickImage();
   }
+
+  _pickImage = async () => {
+    let pickerResult = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    this._handleImagePicked(pickerResult);
+  };
+
+  _handleImagePicked = async pickerResult => {
+    try {
+      this.setState({ uploading: true });
+
+      if (!pickerResult.cancelled) {
+        uploadUrl = await uploadImageAsync(pickerResult.uri);
+        this.setState({ image: uploadUrl });
+      }
+    } catch (e) {
+      console.log(e);
+      alert('Upload failed, sorry :(');
+    } finally {
+      this.setState({ uploading: false });
+    }
+  };
+
+
+async function uploadImageAsync(uri) {
+  // Why are we using XMLHttpRequest? See:
+  // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      resolve(xhr.response);
+    };
+    xhr.onerror = function(e) {
+      console.log(e);
+      reject(new TypeError('Network request failed'));
+    };
+    xhr.responseType = 'blob';
+    xhr.open('GET', uri, true);
+    xhr.send(null);
+  });
+
+  const ref = firebase
+    .storage()
+    .ref("/Images/")
+    .child("abc.jpg");
+  const snapshot = await ref.put(blob);
+
+  // We're done with the blob, close and release it
+  blob.close();
+
+  return await snapshot.ref.getDownloadURL();
+}
+
 
   render() {
     let { image } = this.state;
@@ -161,7 +183,7 @@ export default class profilePageFourth extends React.Component {
           >
             <TouchableOpacity onPress={this._pickImage}>
               {/* ,elevetion:11 */}
-              {this.state.isPicLoaded ? (
+              {this.state.isPicLoaded} ? (
                 <Image
                   style={{
                     height: 120,
@@ -209,6 +231,7 @@ export default class profilePageFourth extends React.Component {
       </View>
     );
   }
+
   componentDidMount() {
     this.getPermissionAsync();
   }
@@ -223,26 +246,22 @@ export default class profilePageFourth extends React.Component {
   };
 
   _pickImage = async () => {
-    // let result = await ImagePicker.launchImageLibraryAsync({
-    //   mediaTypes: ImagePicker.MediaTypeOptions.All,
-    //   allowsEditing: true,
-    //   aspect: [4, 4]
+    
     this.setState({ image: '' })
-
     ImagePicker.launchImageLibrary({}, response  => {
       uploadImage(response.uri)
         .then(url => this.setState({ image: url }))
         .catch(error => console.log(error))
     });
 
-    // console.log(result);
+    console.log(result);
 
-    // if (!result.cancelled) {
-    //   this.setState({ isPicLoaded: false });
-    //   this.setState({ image: result.uri });
-    //  }
+    if (!result.cancelled) {
+      this.setState({ isPicLoaded: false });
+      this.setState({ image: result.uri });
+     }
   };
-}
+
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
@@ -265,3 +284,4 @@ const styles = StyleSheet.create({
     borderRadius: 15
   }
 });
+
