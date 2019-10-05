f3145f3319abe9bc6fa4499c9595da14c6cb231c
AadarshGhodasara <17ce033@charusat.edu.in>
2019-10-05 23:01:30 +0530

    FAQ done , reaming is colour change is required in faq... 😍😍

--- auto-network-app/components/helpSection/FAQ.js
+++ auto-network-app/components/helpSection/FAQ.js
@@ -40,12 +40,48 @@ const CONTENT = [
       "It’s easy to download the application and complete the sign-up process. You can certainly book from your friend's mobile as well. Keep Travelling."
   }
 ];
+const CONTENT_2 = [
+  {
+    title: "What if the auto doesn't show up?",
+    content:
+      "In case the vehicle you booked does not show up, we will offer you a full refund immediately if you have paid online. If paid offline we will cancel the ride on your behalf."
+  },
+  {
+    title: "What if the auto comes up late?",
+    content:
+      "We try our best to ensure our partners reach our customers on time. But in case of delays, do call us and we will help you out by either providing an alternate vehicle or refund of your ride."
+  },
+  {
+    title: "What benefit I will gain using AutoYatri?",
+    content:
+      "With AutoYatri,you can save a lot of money. You won't have to pay more fare for the specific routes even if you are traveling during peak hours or at night. Also, we will be having offers in coming future."
+  },
+  {
+    title:
+      "I want to take the ride individually, is it compulsory to opt for organizational sharing?",
+    content:
+      "No, it’s not compulsory. You can always take the individual ride as per your convenience."
+  },
+  {
+    title:
+      "How can I trust the people that will come with me if I choose for Sharing?",
+    content:
+      "Don’t worry,in case of sharing the passengers will be of the same organization and hence no other passengers can be part of sharing the ride. So don’t be afraid of security issues."
+  },
+  {
+    title: "I want to cancel my booking what I need to do?",
+    content:
+      "You can cancel the ride until the driver arrives at the pick-up location. Once the driver reaches the pickup location you can’t cancel the ride."
+  }
+];
 export default class FAQ extends React.Component {
   constructor() {
     super();
     this.state = {
       activeSections: [],
-      multipleSelect: false
+      multipleSelect: false,
+      activeSections_2: [],
+      multipleSelect_2: false
     };
     this.backEvent = this.backEvent.bind(this);
   }
@@ -80,8 +116,38 @@ export default class FAQ extends React.Component {
       </View>
     );
   }
+
+  setSections_2 = sections => {
+    this.setState({
+      activeSections_2: sections.includes(undefined) ? [] : sections
+    });
+  };
+
+  renderHeader_2 = (section, _, isActive) => {
+    return (
+      <View
+        style={[
+          styles.accordion_header,
+          isActive ? styles.active : styles.inactive
+        ]}
+      >
+        <Text style={styles.accordion_header_text}>{section.title}</Text>
+      </View>
+    );
+  };
+
+  renderContent_2(section, _, isActive) {
+    return (
+      <View
+        style={[styles.content, isActive ? styles.active : styles.inactive]}
+      >
+        <Text style={{ textAlign: "center" }}>{section.content}</Text>
+      </View>
+    );
+  }
   render() {
     const { multipleSelect, activeSections } = this.state;
+    const { multipleSelect_2, activeSections_2 } = this.state;
     return (
       <View style={styles.container}>
         <View style={styles.header}>
@@ -120,19 +186,16 @@ export default class FAQ extends React.Component {
             </View>
           </View>
         </View>
-        <ScrollView style={styles.faq_view} contentContainerStyle={{ flex: 1 }}>
-          <View style={{ marginTop: "3%", flex: 1 }}>
-            <View style={styles.faq_header_view}>
-              <View style={styles.faq_header_inner_view_1}>
-                <Text style={styles.faq_header_text}>Booking Releted</Text>
-              </View>
+        <ScrollView style={styles.faq_view}>
+          {/* <View style={{ marginTop: "3%", flex: 1 }}> */}
+          <View style={styles.faq_header_view}>
+            <View style={styles.faq_header_inner_view_1}>
+              <Text style={styles.faq_header_text}>Booking Releted</Text>
             </View>
-            <View style={styles.faq_booking_releted_view}>
-              {/* <View
-                style={{
-                  
-                }}
-              > */}
+          </View>
+          <View style={styles.faq_booking_releted_view}>
+            <View style={{ flex: 0.2, margin: "3%" }}></View>
+            <View style={{ flex: 0.6 }}>
               <Accordion
                 activeSections={activeSections}
                 sections={CONTENT}
@@ -143,9 +206,31 @@ export default class FAQ extends React.Component {
                 duration={400}
                 onChange={this.setSections}
               />
-              {/* </View> */}
+            </View>
+            <View style={{ flex: 0.2, margin: "3%" }}></View>
+          </View>
+          <View style={styles.passenger_header_view}>
+            <View style={styles.faq_header_inner_view_1}>
+              <Text style={styles.faq_header_text}>Passengers</Text>
             </View>
           </View>
+          <View style={styles.faq_passenger_view}>
+            <View style={{ flex: 0.2, margin: "3%" }}></View>
+            <View style={{ flex: 0.6 }}>
+              <Accordion
+                activeSections={activeSections_2}
+                sections={CONTENT_2}
+                touchableComponent={TouchableOpacity}
+                expandMultiple={multipleSelect_2}
+                renderHeader={this.renderHeader_2}
+                renderContent={this.renderContent_2}
+                duration={400}
+                onChange={this.setSections_2}
+              />
+            </View>
+            <View style={{ flex: 0.2, margin: "3%" }}></View>
+          </View>
+          {/* </View> */}
         </ScrollView>
       </View>
     );
@@ -168,30 +253,47 @@ const styles = StyleSheet.create({
     flex: 0.76
   },
   faq_header_view: {
-    flex: 0.06,
+    flex: 0.3,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    marginTop: "3%"
   },
   faq_header_text: {
     fontSize: 30,
     textAlignVertical: "center",
-    textAlign: "center"
+    textAlign: "center",
+    color: colors.light.white_color
   },
   faq_header_inner_view_1: {
-    borderWidth: 0.5,
     width: "95%",
-    borderRadius: 25
+    flex: 1,
+    // borderWidth: 0.5,
+    borderRadius: 25,
+    backgroundColor: "#45C298"
     // borderBottomWidth: 0.5
   },
   faq_booking_releted_view: {
-    flex: 0.65,
-    backgroundColor: "red",
+    flex: 0.2,
+    backgroundColor: "#185C75",
     alignItems: "center",
     justifyContent: "center",
     width: "95%",
     alignSelf: "center",
     borderRadius: 25,
-    borderWidth: 0.5
+    borderWidth: 0.5,
+    marginTop: "3%"
+  },
+  faq_passenger_view: {
+    flex: 0.2,
+    backgroundColor: "#185C75",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "95%",
+    marginBottom: "3%",
+    alignSelf: "center",
+    borderRadius: 25,
+    borderWidth: 0.5,
+    marginTop: "1%"
   },
   title: {
     textAlign: "center",
@@ -229,5 +331,11 @@ const styles = StyleSheet.create({
     marginTop: "0.5%",
     width: "95%",
     alignSelf: "center"
+  },
+  passenger_header_view: {
+    flex: 0.3,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "10%"
   }
 });
